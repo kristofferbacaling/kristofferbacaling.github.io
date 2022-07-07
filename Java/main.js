54633a9fc22f5cdcce8101370c0f940a795f3d4b
kristofferbacaling <kristofferbacaling@gmail.com>
2022-07-07 17:24:13 +1200

    work on expanding my anwers for loops and array questions

--- Java/main.js
+++ Java/main.js
@@ -7,3 +7,30 @@ function domButton() {
 }
 
 document.getElementById('krisButton').onclick = domButton
+
+let arrayNames = ['Kris', 'Ryan', 'John', 'Alice']
+//                   0       1       2       3
+//This is an array array work like this the fors name in this list is kris but is gvein the index number as 0
+// as array start counting at 0 note that when ask how many names are in this array it will say 4
+
+let objectsInfo = {
+  coustomerOne: {
+    name: 'Kris',
+    email: 'kris@email.com',
+  },
+
+  coustomerTwo: {
+    name: 'Ryan',
+    email: 'ryan@email.com',
+  },
+
+  coustomerThree: {
+    name: 'John',
+    email: 'John@email.com',
+  },
+
+  coustomerFour: {
+    name: 'Alice',
+    email: 'alice@email.com',
+  },
+}
